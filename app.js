3e0d849e7422d6b2cf536b980ec264bccef865f7
ojha13291 <ojha13291@gmail.com>
2025-04-27 14:25:57 +0530

    done animations

---
+++ app.js
@@ -0,0 +1,46 @@
+const video1 = document.getElementById('projectvideo1');
+const video2 = document.getElementById('projectvideo2');
+const video3 = document.getElementById('projectvideo3');
+const hoverSign = document.querySelector('.hover-sign');
+
+const videoList = [video1, video2, video3];
+
+videoList.forEach(function(video){
+    video.addEventListener('mouseover', function(){
+        video.play();
+        hoverSign.classList.add('active')
+    })
+    video.addEventListener('mouseout', function(){
+        video.pause();
+        video.currentTime = 0;
+        hoverSign.classList.remove('active')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
